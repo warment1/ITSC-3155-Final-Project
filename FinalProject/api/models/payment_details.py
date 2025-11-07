fe2783ee49784f375f760e1b3a4ede8752a54481
mikeywheel1000 <michaelwheeler0101@icloud.com>
2025-11-07 18:11:22 -0500

    Create payment_details.py

---
+++ FinalProject/api/models/payment_details.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+
+class Order(Base):
+    __tablename__ = "payment"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    Card_number = Column(Integer)
+    Transaction_status = Column(String(100))
+    Payment_type = Column(String(100))
