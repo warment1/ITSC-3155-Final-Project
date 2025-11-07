a5d67903e9a9391b4897549b20c530fbb67eb526
mikeywheel1000 <michaelwheeler0101@icloud.com>
2025-11-07 18:46:57 -0500

    Create payment_details.py

---
+++ FinalProject/api/schemas/payment_details.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+from .users import User
+
+
+class PaymentDetailBase(BaseModel):
+    Card_number: int
+    Transaction_status: str
+    Payment_type: str
+
+
+class PaymentDetailCreate(PaymentDetailBase):
+    User_id: int
+
+class PaymentDetailUpdate(BaseModel):
+    User_id: Optional[int] = None
+    Card_number: Optional[int] = None
+    Transaction_status: Optional[str] = None
+    Payment_type: Optional[str] = None
+  
+
+
+class PaymentDetail(PaymentDetailBase):
+    Card_number: int
+    Transaction_status: str
+    Payment_type: str
+    User_id: User = None
+
+    class ConfigDict:
+        from_attributes = True
