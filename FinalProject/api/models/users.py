7640e3d04e3729e0f4a27cf49df9a12683930672
mikeywheel1000 <michaelwheeler0101@icloud.com>
2025-11-07 17:59:02 -0500

    Create users.py

---
+++ FinalProject/api/models/users.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+
+class User(Base):
+    __tablename__ = "User"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    customer_name = Column(String(100))
+    Address = Column(String(100)
+    email = Column(String(100)
+    phone_number = Column(Integer)
+    pastOrders = Column(String(300))
+
+    CC_details = relationship("CardDetail", back_populates="user")
