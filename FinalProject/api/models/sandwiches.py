5eb97a30ca9f7655ec84c06de2db9e1f0f4ed1a8
mikeywheel1000 <michaelwheeler0101@icloud.com>
2025-11-07 18:03:32 -0500

    Update sandwiches.py

--- FinalProject/api/models/sandwiches.py
+++ FinalProject/api/models/sandwiches.py
@@ -10,6 +10,8 @@ class Sandwich(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     sandwich_name = Column(String(100), unique=True, nullable=True)
     price = Column(DECIMAL(4, 2), nullable=False, server_default='0.0')
+    calories = Column(Integer)
 
     recipes = relationship("Recipe", back_populates="sandwich")
     order_details = relationship("OrderDetail", back_populates="sandwich")
+    Allergies = relationship("AllergySum", back_populates="sandwich")
