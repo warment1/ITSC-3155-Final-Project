af86de3b8bf4574e82f15f5f7d03765458e3dd93
mikeywheel1000 <michaelwheeler0101@icloud.com>
2025-11-12 14:58:25 -0500

    Fixed bug with payment_details not loading

--- FinalProject/api/models/model_loader.py
+++ FinalProject/api/models/model_loader.py
@@ -1,13 +1,14 @@
-from . import orders, order_details, recipes, sandwiches, resources,users
+from . import orders, order_details, recipes, sandwiches, resources,users, payment_details
 
 from ..dependencies.database import engine
 
 
 def index():
+    payment_details.Base.metadata.create_all(engine)
+    users.Base.metadata.create_all(engine)
     orders.Base.metadata.create_all(engine)
     order_details.Base.metadata.create_all(engine)
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
-    users.Base.metadata.create_all(engine)
-    payment_details.Base.metadata.create_all(engine)
+
--- FinalProject/api/models/users.py
+++ FinalProject/api/models/users.py
@@ -5,12 +5,12 @@ from ..dependencies.database import Base
 
 
 class User(Base):
-    __tablename__ = "User"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     customer_name = Column(String(100))
-    Address = Column(String(100)
-    email = Column(String(100)
+    Address = Column(String(100))
+    email = Column(String(100))
     phone_number = Column(Integer)
     pastOrders = Column(String(300))
 
