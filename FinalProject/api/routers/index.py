1091b528d6252666583ad6929e78753a754e738c
warment1 <warment1@charlotte.edu>
2025-11-29 16:30:29 -0500

    Update index.py

--- FinalProject/api/routers/index.py
+++ FinalProject/api/routers/index.py
@@ -1,6 +1,12 @@
-from . import orders, order_details
+from . import orders, order_details, recipes, payment_details, sandwiches, users, resources
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
+    app.include_router(recipes.router)
+    app.include_router(payment_details.router)
+    app.include_router(sandwiches.router)
+    app.include_router(users.router)
+    app.include_router(resources.router)
+
