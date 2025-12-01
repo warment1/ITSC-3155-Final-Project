97f6c7388f41a6a78b1bf9e782d2cf6611a239d1
warment1 <warment1@charlotte.edu>
2025-12-01 15:44:27 -0500

    Update model_loader.py

--- FinalProject/api/models/model_loader.py
+++ FinalProject/api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources,users, payment_details
+from . import orders, order_details, recipes, sandwiches, resources,users, payment_details, reviews, users
 
 from ..dependencies.database import engine
 
@@ -11,4 +11,6 @@ def index():
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    reviews.Base.metadata.create_all(engine)
+
 
