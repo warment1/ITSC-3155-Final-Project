49175b991d6e450fcc3ad568771d95acf1b2aefb
warment1 <warment1@charlotte.edu>
2025-12-02 20:49:22 -0500

    Update config.py

--- FinalProject/api/dependencies/config.py
+++ FinalProject/api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "Labradoodle5!"
+    db_password = "rootroot"
     app_host = "localhost"
     app_port = 8000
