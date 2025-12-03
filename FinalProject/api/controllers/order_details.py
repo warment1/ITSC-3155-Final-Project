e3af4b8d9f1c7b048726bc3df42b454aa109d220
warment1 <warment1@charlotte.edu>
2025-12-02 19:37:54 -0500

    Update order_details.py

--- FinalProject/api/controllers/order_details.py
+++ FinalProject/api/controllers/order_details.py
@@ -33,7 +33,7 @@ def read_all(db: Session):
 
 def read_one(db: Session, item_id):
     try:
-        item = db.query(model.OrderDetail).filter(model.OrderDetail.id == item_id).first()
+        item = db.query(model.OrderDetail).filter(model.OrderDetail.detail_id == item_id).first()
         if not item:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
     except SQLAlchemyError as e:
@@ -44,7 +44,7 @@ def read_one(db: Session, item_id):
 
 def update(db: Session, item_id, request):
     try:
-        item = db.query(model.OrderDetail).filter(model.OrderDetail.id == item_id)
+        item = db.query(model.OrderDetail).filter(model.OrderDetail.detail_id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         update_data = request.dict(exclude_unset=True)
@@ -58,7 +58,7 @@ def update(db: Session, item_id, request):
 
 def delete(db: Session, item_id):
     try:
-        item = db.query(model.OrderDetail).filter(model.OrderDetail.id == item_id)
+        item = db.query(model.OrderDetail).filter(model.OrderDetail.detail_id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         item.delete(synchronize_session=False)
