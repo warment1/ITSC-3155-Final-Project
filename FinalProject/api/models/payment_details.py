a14357a3faea4dc259e051a1aac40795141a4887
warment1 <warment1@charlotte.edu>
2025-12-02 19:41:06 -0500

    Update payment_details.py

--- FinalProject/api/models/payment_details.py
+++ FinalProject/api/models/payment_details.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME, BigInteger
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
@@ -8,7 +8,9 @@ class Payment(Base):
     __tablename__ = "payment"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    Card_number = Column(Integer)
+    Card_number = Column(BigInteger)
     Transaction_status = Column(String(100))
     Payment_type = Column(String(100))
     User_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="CC_details")
